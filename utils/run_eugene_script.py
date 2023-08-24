03e010a685dd614ecce73877444105e607c3c6cc
chris-krenz <ckrenz@bu.edu>
2023-08-24 13:30:29 -0400

    Changed to png generation

--- utils/run_eugene_script.py
+++ utils/run_eugene_script.py
@@ -5,8 +5,14 @@ These part orders are then used for SBOL and other file generation.
 TODO: Ensure JVM properly started on Docker.
 """
 
-import py4j
-
 from py4j.java_gateway import JavaGateway
 
-gateway = JavaGateway()
+gateway = JavaGateway()                   # connect to the JVM
+random = gateway.jvm.java.util.Random()   # create a java.util.Random instance
+number1 = random.nextInt(10)              # call the Random.nextInt method
+number2 = random.nextInt(10)
+print(number1, number2)
+
+addition_app = gateway.entry_point               # get the AdditionApplication instance
+value = addition_app.addition(number1, number2)  # call the addition method
+print(value)
