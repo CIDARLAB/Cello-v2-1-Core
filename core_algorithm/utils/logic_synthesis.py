228bf6ec4a957c0e84619686afbfbc616a014583
chris-krenz <chris.dean.krenz@gmail.com>
2023-12-01 14:15:37 -0500

    fixing v_name bug...

--- core_algorithm/utils/logic_synthesis.py
+++ core_algorithm/utils/logic_synthesis.py
@@ -38,7 +38,7 @@ def call_YOSYS(in_path=None, out_path=None, v_name=None, choice=0, no_files=Fals
         new_in = in_path
 
     # Construct the path to the Verilog file
-    verilog = v_name
+    verilog = v_name  # CRIT: ADD + '.v'?
     v_loc = os.path.join(new_in, verilog)
 
     # Logging the information
