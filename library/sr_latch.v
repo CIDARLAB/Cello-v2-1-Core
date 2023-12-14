9c0c11ef5d68233ea4efd27bd57e8d5baba7b1bd
chris-krenz <chris.dean.krenz@gmail.com>
2023-12-13 19:54:14 -0500

    Removed unneeded verilogs

--- library/sr_latch.v
+++
@@ -1,9 +0,0 @@
-// file: sr_latch.v
-// Using Verilog to describe our SR Latch
-module sr_latch(
-    input wire S, R,
-    output wire Q, Q_not);
-
-    assign Q     = ~(R | Q_not);
-    assign Q_not = ~(S | Q);
-endmodule
