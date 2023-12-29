40ece9c7c169fd5deb8fe33f899a03c7c9e1c5f7
chris-krenz <ckrenz@bu.edu>
2023-12-29 15:03:45 -0500

    run.py check if gateway running

--- run.py
+++ run.py
@@ -1,6 +1,16 @@
 from app.cli import start_cli
 from core_algorithm.utils.py4j_gateway.gateway import start_gateway
+from py4j.java_gateway import JavaGateway
 
 if __name__ == '__main__':
-    start_gateway()
+    try:
+        gateway = JavaGateway(eager_load=True)
+        print("\nJava Py4J gateway already started")
+    except Exception:
+        print("\nAttempting to start Java Py4J gateway...")
+        try:
+            start_gateway()
+            print("Started Java Py4J gateway")
+        except Exception:
+            print("Failed to start Java Py4J gateway!")
     start_cli()
